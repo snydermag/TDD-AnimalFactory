fe6e6bc7ecac2a1ff655dd2cfec76d0cd906c8c0
maggie <snydermag@gmail.com>
2023-07-12 09:42:30 -0400

    AnimalFactory tests completed

--- src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
+++ src/test/java/rocks/zipcodewilmington/AnimalFactoryTest.java
@@ -1,9 +1,39 @@
 package rocks.zipcodewilmington;
 
+import org.junit.Assert;
+import org.junit.Test;
+import rocks.zipcodewilmington.animals.Animal;
+import rocks.zipcodewilmington.animals.animal_creation.AnimalFactory;
+import rocks.zipcodewilmington.animals.Dog;
+import rocks.zipcodewilmington.animals.Cat;
+import java.util.Date;
+
+
+
 /**
  * @author leon on 4/19/18.
  */
 public class AnimalFactoryTest {
-    //TODO - Create Test for `Animal createDog(String name, Date birthDate)`
-    //TODO - Create Test for `Animal createCat(String name, Date birthDate)`
+
+    @Test
+    public void createDogTest(){
+        String name = "Milo";
+        Date date = new Date(2021, 12, 20);
+
+        Dog dog = AnimalFactory.createDog(name, date);
+
+        Assert.assertTrue(dog instanceof Animal);
+
+    }
+
+    @Test
+    public void createCatTest(){
+        String name = "Zula";
+        Date date = new Date(2022, 3, 1);
+
+        Cat cat = AnimalFactory.createCat(name, date);
+
+        Assert.assertTrue(cat instanceof Animal);
+
+    }
 }
